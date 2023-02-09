9d028d09bc5ece50b8244f9ee43d8c6adcb6df50
Santiago Fernández López <119003028+sfdezlop@users.noreply.github.com>
2023-02-09 07:11:24 +0100

    Update App.tsx
    I'm lost in Typing: Cannot find module '../../mocks/characters' or its corresponding type declarations

--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <h2>Week 4 Challenge 3: Games of Trones in React</h2>
+      <h3>I'm lost in Typing: Cannot find module '../../mocks/characters' or its
+        corresponding type declarations</h3>
     </div>
   );
 }
